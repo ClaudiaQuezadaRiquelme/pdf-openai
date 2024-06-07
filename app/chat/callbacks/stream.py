5cfd971061df76b0b3fa8ab781ec6ad8089318a6
ClaudiaQuezadaRiquelme <ca.quezada.riquelme@gmail.com>
2024-06-07 15:54:29 -0400

    completing streaming

--- app/chat/callbacks/stream.py
+++ app/chat/callbacks/stream.py
@@ -1,11 +1,25 @@
+from typing import Any, Dict, List
+from uuid import UUID
 from langchain.callbacks.base import BaseCallbackHandler
+from langchain_core.messages import BaseMessage
 
 class StreamingHandler(BaseCallbackHandler):
     def __init__(self, queue):
         self.queue = queue
+        self.streaming_run_id = set()
+
+    def on_chat_model_start(self, serialized, messages, run_id, **kwargs):
+        if serialized['kwargs']['streaming']:
+            # print('This is a streaming model! I should listen to events with a run_id of ', run_id)
+            self.streaming_run_id.add(run_id)
+
     def on_llm_new_token(self, token, **kwargs):
         self.queue.put(token)
-    def on_llm_end(self, response, **kwargs):
-        self.queue.put(None)
+
+    def on_llm_end(self, response, run_id, **kwargs):
+        if run_id in self.streaming_run_id:
+            self.queue.put(None)
+            self.streaming_run_id.remove(run_id)
+
     def on_llm_error(self, error, **kwargs):
         self.queue.put(None)
